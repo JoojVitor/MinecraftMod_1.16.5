200c381ae298baf41eaea6d3f9987633baf4f5f7
JoojVitor <jv-santos98@hotmail.com>
2021-11-21 18:43:49 -0300

    Add tooltip for Levitation Wand

--- src/main/java/com/joojvitor/firstmod/item/custom/LevitationWand.java
+++ src/main/java/com/joojvitor/firstmod/item/custom/LevitationWand.java
@@ -1,5 +1,7 @@
 package com.joojvitor.firstmod.item.custom;
 
+import com.joojvitor.firstmod.util.InputHelper;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -8,8 +10,12 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
+
+import java.util.List;
+
 public class LevitationWand extends Item {
     public LevitationWand(Properties properties) {
         super(properties);
@@ -40,5 +46,16 @@ public class LevitationWand extends Item {
         }
 
         return super.hitEntity(stack, target, attacker);
+    }
+
+    public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
+        if (InputHelper.isHoldingShift()) {
+            tooltip.add(new StringTextComponent("Levitate the target when attack;\n" +
+                    "CROUCH and RIGHT-CLICK to self levitate."));
+        } else {
+            tooltip.add(new StringTextComponent("Hold \u00A7eSHIFT\u00A7r for more information"));
+        }
+
+        super.addInformation(stack, world, tooltip, flag);
     }
 }
